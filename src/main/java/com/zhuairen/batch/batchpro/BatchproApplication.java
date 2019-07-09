a6072c30d47435d5427b46c623061255f58ad7f5
MacBook Pro <MF840@MacBookdeMacBook-Pro.local>
2019-07-09 23:32:14 +0800

    批量自动处理start

---
+++ src/main/java/com/zhuairen/batch/batchpro/BatchproApplication.java
@@ -0,0 +1,13 @@
+package com.zhuairen.batch.batchpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchproApplication.class, args);
+	}
+
+}
